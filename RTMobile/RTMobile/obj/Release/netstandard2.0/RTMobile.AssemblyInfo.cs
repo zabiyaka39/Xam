8e8087e70e5a2bf6dbaba49a5ae2fcb19ec5769b
Секисов Владислав <sekisov@rosohrana.ru>
2020-03-04 15:42:32 +0300

    Изменена основная конфигурация проекта

--- RTMobile/RTMobile/obj/Release/netstandard2.0/RTMobile.AssemblyInfo.cs
+++ RTMobile/RTMobile/obj/Release/netstandard2.0/RTMobile.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Секисов Владислав Александрович")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Приложение для работы в системе учета задач Jira")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.2")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RTMobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RTMobile")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.2")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
